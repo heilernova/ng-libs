7a063bb516dacc29c03fa7876d40722e039f2458
Heile Nova <heilernova@outlook.com>
2022-09-20 13:48:19 -0500

    Actualizacion

--- projects/app-testing/src/app/inputs/inputs.component.ts
+++ projects/app-testing/src/app/inputs/inputs.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import { InputsModule } from 'ng-nova';
-import { InputNumber, InputsModule } from 'projects/ng-nova/src/public-api';
+import { InputNumber, InputsModule, MessageBoxModule } from 'projects/ng-nova/src/public-api';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inputs',
   standalone: true,
-  imports: [CommonModule, InputsModule, MatInputModule,ReactiveFormsModule ],
+  imports: [CommonModule, InputsModule, MatInputModule,ReactiveFormsModule, MessageBoxModule ],
   templateUrl: './inputs.component.html',
   styleUrls: ['./inputs.component.scss']
 })
